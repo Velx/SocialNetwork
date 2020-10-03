44814b660cf65bc7c76028e1ec036892fde26001
Velx <feofan139@hotmail.com>
2020-10-03 23:22:26 +0300

    Added view for user creation (registration)

--- users/views.py
+++ users/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework.permissions import AllowAny
+from rest_framework.generics import CreateAPIView
+from .models import User
+from .serializers import UserSerializer
 
-# Create your views here.
+
+class CreateUserView(CreateAPIView):
+    model = User
+    serializer_class = UserSerializer
+    permission_classes = [AllowAny]
+
