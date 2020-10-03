a1d068253f519e3912222299603613a8b4e30986
Velx <feofan139@hotmail.com>
2020-10-03 21:47:45 +0300

    Added settings for auth through DRF-SimpleJWT lib

--- socialnetwork/settings.py
+++ socialnetwork/settings.py
@@ -124,3 +124,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# Rest Framework settings
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
