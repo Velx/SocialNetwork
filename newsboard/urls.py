c8d001c8c75fdcf799cf725413ae44f307187cdb
Velx <feofan139@hotmail.com>
2020-10-04 00:00:43 +0300

    Completed routing for project. Users urls to newsboard urls to project root url as /api

---
+++ newsboard/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path, include
+from rest_framework.routers import SimpleRouter
+
+from .views import PostViewSet
+from users.urls import urlpatterns as users_urls
+
+router = SimpleRouter()
+router.register(r'', PostViewSet)
+
+urlpatterns = [
+    path('posts', include(router.urls)),
+    path('', include(users_urls))
+]
