47ab2f4b6ff67d147be9f91c27b00bb5f76dc583
Velx <feofan139@hotmail.com>
2020-10-04 18:36:42 +0300

    Fixed middleware

--- users/middlewares.py
+++ users/middlewares.py
@@ -2,7 +2,16 @@ from django.utils import timezone
 from django.core.cache import cache
 
 
-class UpdateLastActivityMiddleware(object):
-    def process_view(self, request, view_func, view_args, view_kwargs):
-        if hasattr(request, 'user') and request.user.is_authenticated():
+class UpdateLastActivityMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        response = self.get_response(request)
+        return response
+
+    @staticmethod
+    def process_view(request, view_func, view_args, view_kwargs):
+        if hasattr(request, 'user') and request.user.is_authenticated:
             cache.set(request.user.username, timezone.now())
+        return None
