220485835b0a052b2a92c23539b284f05982c2d8
Velx <feofan139@hotmail.com>
2020-10-04 21:52:35 +0300

    Added endpoints to like and unlike post

--- newsboard/views.py
+++ newsboard/views.py
@@ -1,5 +1,7 @@
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from .models import Post
 from .serializers import PostSerializer
 
@@ -14,3 +16,17 @@ class PostViewSet(ModelViewSet):
         else:
             permission_classes = [IsAuthenticated]
         return [permission() for permission in permission_classes]
+
+    @action(detail=True, methods=['POST'])
+    def like(self, request, pk=None):
+        post = self.get_object()
+        post.liked_by.add(request.user)
+        serializer = self.get_serializer(post)
+        return Response(serializer.data)
+
+    @action(detail=True, methods=['POST'])
+    def unlike(self, request, pk=None):
+        post = self.get_object()
+        post.liked_by.remove(request.user)
+        serializer = self.get_serializer(post)
+        return Response(serializer.data)
