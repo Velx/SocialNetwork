249623065831a4ef17aaaf918c999a529dc8de7a
Velx <feofan139@hotmail.com>
2020-10-05 13:01:44 +0300

    Added through table to track date when like was added

--- newsboard/models.py
+++ newsboard/models.py
@@ -8,7 +8,7 @@ class Post(models.Model):
     author = models.ForeignKey(user_model, on_delete=models.CASCADE, verbose_name='posts')
     title = models.CharField(max_length=70)
     text = models.TextField()
-    liked_by = models.ManyToManyField(user_model, related_name='likes')
+    liked_by = models.ManyToManyField(user_model, related_name='likes', through='LikedBy')
     creation_date = models.DateTimeField(auto_now=True)
 
     @property
@@ -17,3 +17,12 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class LikedBy(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    user = models.ForeignKey(user_model, on_delete=models.CASCADE)
+    date = models.DateField(auto_now=True)
+
+    class Meta:
+        unique_together = (('post', 'user'),)
