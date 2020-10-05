c4aa48270fb3e2b726094f24d0921b92d5cf7f0d
Velx <feofan139@hotmail.com>
2020-10-05 13:35:25 +0300

    Code cleanup: disabled not needed methods for PostViewSet

--- newsboard/views.py
+++ newsboard/views.py
@@ -1,14 +1,14 @@
 from datetime import datetime, timedelta
-from rest_framework.viewsets import ModelViewSet, GenericViewSet
+from rest_framework.viewsets import GenericViewSet
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.decorators import action
 from rest_framework.response import Response
-from rest_framework.mixins import ListModelMixin
+from rest_framework.mixins import ListModelMixin, RetrieveModelMixin, CreateModelMixin
 from .models import Post, LikedBy
 from .serializers import PostSerializer, PostAnalyticsSerializer
 
 
-class PostViewSet(ModelViewSet):
+class PostViewSet(CreateModelMixin, ListModelMixin, RetrieveModelMixin, GenericViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
